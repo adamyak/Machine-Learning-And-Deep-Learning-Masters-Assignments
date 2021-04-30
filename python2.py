482b8a15dc771a4d4c7587b4673225c1716dd152
adamyak <53369543+adamyak@users.noreply.github.com>
2021-05-01 00:05:26 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,23 @@
+
+# Create a pattern with nested for loop in Python
+i=0
+j=5
+while i != 5:
+    print("* "*i)
+    i=i+1
+while j!=0:
+     print("* "*j)
+     j=j-1
+    
+    
+
+
+# Write a python program to reverse a word after accepting the input from user.
+string=str(input())
+print(string[::-1])
+
+#print(string[-1:string])     
+    
+    
+
+
